0915a872d05a514adaf4f07cdedd8f8db98b0b77
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-09-13 00:19:45 -1000

    Created Parent Directory

--- exercise_1.1/add.py
+++
@@ -1,9 +0,0 @@
-# Get input from the user for 'a' and 'b'
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-
-# Add 'a' and 'b' to get the value of 'c'
-c = a + b
-
-# Print the value of 'c'
-print("The sum of {} and {} is: {}".format(a, b, c))
